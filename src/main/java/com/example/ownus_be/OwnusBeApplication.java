3f110ce9b55062b79924a2f29dc2671f3d764c24
4Sangwon <idcop7@naver.com>
2023-03-10 20:48:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/ownus_be/OwnusBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ownus_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnusBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnusBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ownus/ownus_be/OwnusBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ownus.ownus_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnusBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
